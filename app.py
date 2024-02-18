004aee4fff889bb89c1feabb85544afba49c0986
sockomode <eli123013@gmail.com>
2024-02-17 17:13:26 -0800

    made airports and index pages

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask.templating import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/airports')
+def airports():
+    return render_template('airports.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
